7d1fd39be66fb8fd9644b5d6f9a132b0f2a5568e
unknown <gb.lee@dbfis.co.kr>
2024-09-12 14:35:12 +0900

    Feat: react-router-dom 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
