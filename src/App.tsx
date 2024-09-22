e1df1224dfc9a478451a68ba9a70abb177524ec3
gyubinlee-dbfis <gb.lee@dbfis.co.kr>
2024-09-23 00:47:46 +0900

    Feat: 대시보드 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Menu from './components/Menu';
+import styles from './styles/App.module.css';
+import Dashboard from './pages/Dashboard';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-      </Routes>
+      <div className={styles.container}>
+        {/* 왼쪽 고정 메뉴바 */}
+        <Menu />
+        
+        {/* 오른쪽 컨텐츠 영역 */}
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
