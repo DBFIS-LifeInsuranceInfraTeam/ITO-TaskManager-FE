44ca00a3be1c83639b9365892442e4521895027f
unknown <gb.lee@dbfis.co.kr>
2024-09-27 14:23:47 +0900

    Feat: Login&Signup 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,55 @@ import Home from './pages/Home';
 import Menu from './components/Menu';
 import styles from './styles/App.module.css';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+// const App: React.FC = () => {
+  
+//   return (
+//     <Router>
+//       <div className={styles.container}>
+//         {/* 왼쪽 고정 메뉴바 */}
+//         <Menu />
+        
+//         {/* 오른쪽 컨텐츠 영역 */}
+//         <div className={styles.content}>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/dashboard" element={<Dashboard />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/signup" element={<Signup />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// };
+
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className={styles.container}>
-        {/* 왼쪽 고정 메뉴바 */}
-        <Menu />
-        
-        {/* 오른쪽 컨텐츠 영역 */}
-        <div className={styles.content}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+        <Route path="/dashboard" element={<MainLayout><Dashboard /></MainLayout>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </Router>
   );
 };
+
+const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Menu />
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
 
 export default App;
