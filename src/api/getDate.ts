3b321bb18e91fb378ccc0252235cff82596efe12
gyubinlee-dbfis <gb.lee@dbfis.co.kr>
2024-10-28 17:03:14 +0900

    Update getDate.ts

--- src/api/getDate.ts
+++ src/api/getDate.ts
@@ -3,9 +3,10 @@ import apiClient from '../services/apiClient';
 export const getDate = async () => {
   try {
     const response = await apiClient.get(`/current-datetime`);
-    //console.log(response.data.year);
+    console.log(response);
     return response.data;
   } catch (error) {
+    console.error("Error fetching date:", error);
     throw error;
   }
 };
